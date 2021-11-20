882cef2dd868a809ea81ce00100aaf4e461eb4af
luan <oga.tatsumi1230410@gmail.com>
2021-11-20 11:27:45 +0700

    Add project files.

---
+++ A Number Guessing Game/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Number_Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int winnumber = rnd.Next(1, 100);
+            Console.Write("Please enter number: ");
+            int n = int.Parse(Console.ReadLine());
+            int times = 1;
+            while(n != winnumber && times <= 7)
+            {
+                if (n > winnumber)
+                {
+                    Console.WriteLine("Wrong! The win number is lower then your number");
+                    Console.Write("Please enter number again: ");
+                    n = int.Parse(Console.ReadLine());
+                    times++;
+                }  
+                else if (n < winnumber)
+                {
+                    Console.WriteLine("Wrong! The win number is higher then your number");
+                    Console.Write("Please enter number again: ");
+                    n = int.Parse(Console.ReadLine());
+                    times++;
+                }
+            }
+            if(times > 7 && n != winnumber)
+            {
+                Console.Write($"Sorry!! The win number is {winnumber}");
+            }
+            else
+            {
+                Console.Write($"Congratulation!! The win number is {n}");
+            } 
+                
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Check Prime/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+            Console.Write("Enter number: ");
+            n = int.Parse(Console.ReadLine());
+            int m = n / 2;
+            int temp = 0;
+            for (i = 2; i <= m; i++)
+            {
+                if (n % i == 0)
+                {
+                    temp = 1;
+                }
+            }
+            if (temp != 1)
+            {
+                Console.Write("Number is Prime.");
+            }
+            else
+            {
+                Console.Write("Number is not Prime.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
